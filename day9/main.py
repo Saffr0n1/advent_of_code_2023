b9420353698e7ee075ccee0e52df27694af912eb
Abishrant Panday <abishrantpanday@college.harvard.edu>
2023-12-10 18:10:41 -0600

    day9

---
+++ day9/main.py
@@ -0,0 +1,36 @@
+def consecutive_difference(num_list):
+    diff_list = list(zip(num_list, num_list[1:]))
+    return [nums[1] - nums[0] for nums in diff_list]
+
+
+def reduce(num_list):
+    ends = []
+    ends.append(num_list[-1])
+    while set(num_list) != {0}:
+        num_list = consecutive_difference(num_list)
+        ends.append(num_list[-1])
+    return sum(ends)
+
+
+def reduced_sum(input):
+    out_sum = 0
+    for num_list in input:
+        out_sum += reduce(num_list)
+    return out_sum
+
+
+if __name__ == "__main__":
+    fpath = r"C:\Users\Abi\Documents\GitHub\advent_of_code_2023\day9\input.txt"
+    with open(fpath, "r") as file:
+        input = [
+            (line[:-1].split() if "\n" in line else line.split())
+            for line in file.readlines()
+        ]
+    input = [[int(num) for num in sublist] for sublist in input]
+
+    # Part 1
+    print(f"Part 1 solution: {reduced_sum(input)}")
+
+    # Part 2
+    rev_input = [num_list[::-1] for num_list in input]
+    print(f"Part 2 solution: {reduced_sum(rev_input)}")
