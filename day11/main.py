bf7a3488d95f79284adb8c9026f683bb0c18e528
Saffron <abishrantpanday@college.harvard.edu>
2024-01-02 22:35:44 -0500

    Directory creation and days 11, 12

---
+++ day11/main.py
@@ -0,0 +1,87 @@
+def expand_universe(universe, scale_factor):
+    expanded_universe = [row for row in universe]
+    rows = []
+    badCols = set()
+    for row in range(len(expanded_universe)):
+        emptyRow = True
+        for col in range(len(expanded_universe[0])):
+            if expanded_universe[row][col] == "#":
+                emptyRow = False
+                badCols.add(col)
+        if emptyRow:
+            rows.append(row)
+
+    cols = [col for col in range(len(expanded_universe[0])) if col not in badCols]
+
+    dotRow = "." * (len(expanded_universe[0]) + len(cols) * (scale_factor - 1))
+    for col in sorted(cols, reverse=True):
+        for row in range(len(expanded_universe)):
+            expanded_universe[row] = (
+                expanded_universe[row][:col]
+                + "." * scale_factor
+                + expanded_universe[row][col + 1 :]
+            )
+
+    insertRows = [dotRow] * scale_factor
+
+    for row in sorted(rows, reverse=True):
+        expanded_universe = (
+            expanded_universe[:row] + insertRows + expanded_universe[row + 1 :]
+        )
+
+    return expanded_universe, sorted(rows), sorted(cols)
+
+
+def get_initial_galaxies(universe):
+    galaxies = []
+    for row in range(len(universe)):
+        for col in range(len(universe[0])):
+            if universe[row][col] == "#":
+                galaxies.append([row, col])
+    return galaxies
+
+
+def get_galaxies(initial_galaxies, rows, cols, scale_factor):
+    galaxies = []
+    for galaxy in initial_galaxies:
+        gal_x, gal_y = galaxy[0], galaxy[1]
+        for row in rows:
+            if galaxy[0] > row:
+                gal_x += scale_factor - 1
+        for col in cols:
+            if galaxy[1] > col:
+                gal_y += scale_factor - 1
+        galaxies.append([gal_x, gal_y])
+
+    return galaxies
+
+
+def all_pairs_galaxy_distances(galaxies):
+    totDist = 0
+    for gal_1 in range(len(galaxies)):
+        for gal_2 in range(gal_1 + 1, len(galaxies)):
+            totDist += abs(galaxies[gal_1][0] - galaxies[gal_2][0]) + abs(
+                galaxies[gal_1][1] - galaxies[gal_2][1]
+            )
+
+    return totDist
+
+
+if __name__ == "__main__":
+    fpath = r"/Users/saffron/Documents/Github/advent_of_code_2023/day11/input.txt"
+    with open(fpath, "r") as file:
+        input = [(line[:-1] if "\n" in line else line) for line in file.readlines()]
+
+    initial_galaxies = get_initial_galaxies(input)
+
+    # PART 1
+    SCALE_FACTOR = 2
+    universe, rows, cols = expand_universe(input, SCALE_FACTOR)
+    galaxies = get_galaxies(initial_galaxies, rows, cols, SCALE_FACTOR)
+    print(f"Part 1 solution: {all_pairs_galaxy_distances(galaxies)}")
+
+    # PART 2
+    SCALE_FACTOR = 1_000_000
+    universe, rows, cols = expand_universe(input, SCALE_FACTOR)
+    galaxies = get_galaxies(initial_galaxies, rows, cols, SCALE_FACTOR)
+    print(f"Part 2 solution: {all_pairs_galaxy_distances(galaxies)}")
---
+++ day12/main.py
@@ -0,0 +1,130 @@
+import re
+from functools import cache
+from time import time
+
+
+def brute_force(input_str, groups, current_string="", index=0):
+    if index == len(input_str):
+        return 1 if validate_arrangement(current_string, groups) else 0
+
+    if input_str[index] == "?":
+        return brute_force(
+            input_str, groups, current_string + ".", index + 1
+        ) + brute_force(input_str, groups, current_string + "#", index + 1)
+    else:
+        return brute_force(
+            input_str, groups, current_string + input_str[index], index + 1
+        )
+
+
+def validate_arrangement(input, groups):
+    pattern = re.compile(r"(#+.)")
+    matches = [match[1][:-1] for match in pattern.finditer(input)]
+
+    if len(groups) != len(matches):
+        return False
+
+    for i in range(len(groups)):
+        if groups[i] != len(matches[i]):
+            return False
+
+    return True
+
+
+manual_cache = {}
+
+
+def manual_cached_count(input, groups):
+    if input == "":
+        return 1 if not groups else 0
+    if not groups:
+        return 1 if "#" not in input else 0
+
+    if (input, groups) in manual_cache:
+        return manual_cache[(input, groups)]
+
+    count = 0
+
+    if input[0] in ".?":
+        count += manual_cached_count(input[1:], groups)
+
+    if input[0] in "#?":
+        group_len = groups[0]
+        if (
+            group_len <= len(input)
+            and "." not in input[:group_len]
+            and (group_len == len(input) or input[group_len] != "#")
+        ):
+            count += manual_cached_count(input[group_len + 1 :], groups[1:])
+
+    manual_cache[(input, groups)] = count
+
+    return count
+
+
+@cache
+def cached_count(input, groups):
+    if input == "":
+        return 1 if not groups else 0
+    if not groups:
+        return 1 if "#" not in input else 0
+
+    count = 0
+
+    if input[0] in ".?":
+        count += cached_count(input[1:], groups)
+
+    if input[0] in "#?":
+        group_len = groups[0]
+        if (
+            group_len <= len(input)
+            and "." not in input[:group_len]
+            and (group_len == len(input) or input[group_len] != "#")
+        ):
+            count += cached_count(input[group_len + 1 :], groups[1:])
+
+    return count
+
+
+if __name__ == "__main__":
+    fpath = r"/Users/saffron/Documents/Github/advent_of_code_2023/day12/input.txt"
+    with open(fpath, "r") as file:
+        input = [
+            (line[:-1].split() if "\n" in line else line.split())
+            for line in file.readlines()
+        ]
+    input = [[inp[0] + ".", tuple(int(x) for x in inp[1].split(","))] for inp in input]
+    unfolded_input = [[((inp[0][:-1] + "?") * 5)[:-1], inp[1] * 5] for inp in input]
+
+    # PART 1
+    s_bf = time()
+    part1 = 0
+    for line in input:
+        part1 += brute_force(line[0], line[1])
+    e_bf = time()
+    print(f"Part 1 brute force solution: {part1} with time {e_bf - s_bf}")
+
+    # PART 1
+    s_c_1 = time()
+    part1 = 0
+    for line in input:
+        part1 += cached_count(line[0], line[1])
+    e_c_1 = time()
+    print(f"Part 1 cached solution: {part1} with time {e_c_1 - s_c_1}")
+
+    # PART 2
+    s_mc = time()
+    part2 = 0
+    for line in unfolded_input:
+        part2 += manual_cached_count(line[0], line[1])
+    e_mc = time()
+
+    print(f"Part 2 manual cache solution: {part2} with runtime {e_mc - s_mc}")
+
+    # PART 2
+    s_c_2 = time()
+    part2 = 0
+    for line in unfolded_input:
+        part2 += cached_count(line[0], line[1])
+    e_c_2 = time()
+    print(f"Part 2 cached solution: {part2} with runtime {e_c_2 - s_c_2}")
